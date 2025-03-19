903506b95210739e84be19fb97c02b66f80de478
Carrick <carrick.entwistle2001@gmail.com>
2025-03-19 11:00:32 +0000

    Enable DEBUG mode for development and add WorkoutSession and ExerciseLog models with migration

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Exercise
+from .models import Exercise, WorkoutSession, ExerciseLog
 
 # Register your models here.
 admin.site.register(Exercise)
+admin.site.register(WorkoutSession)
+admin.site.register(ExerciseLog)
--- workouts/models.py
+++ workouts/models.py
@@ -15,3 +15,30 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class WorkoutSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link workout to a specific user
+    title = models.CharField(max_length=200)  # Title for the workout session
+    date = models.DateField(auto_now_add=True)  # Automatically set to current date
+    exercises = models.ManyToManyField(Exercise, through='ExerciseLog')  # Many to Many with Exercise, through a join model
+    total_duration = models.IntegerField()  # Duration of the workout in minutes
+    date_completed = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["-date_completed"]
+    
+    def __str__(self):
+        return f"Workout: {self.title} completed by {self.user}"
+    
+
+
+class ExerciseLog(models.Model):
+    workout_session = models.ForeignKey(WorkoutSession, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2)  # Weight lifted (kg or lbs)
+    
+    def __str__(self):
+        return f"{self.exercise.name} - {self.sets} sets of {self.reps} reps"
