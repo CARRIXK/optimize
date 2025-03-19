ecb119b362a73b93a7842b3da0d4dbac3808a716
Carrick <carrick.entwistle2001@gmail.com>
2025-03-19 10:29:46 +0000

    Refactor environment configuration to use SECRET_KEY from environment variables

--- env.py
+++ env.py
@@ -3,3 +3,8 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_d0nUMl1bGVKu@ep-steep-water-a2j495jf.eu-central-1.aws.neon.tech/cross_essay_ream_622173")
 
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_d0nUMl1bGVKu@ep-steep-water-a2j495jf.eu-central-1.aws.neon.tech/cross_essay_ream_622173")
+os.environ.setdefault("SECRET_KEY", "48FFC12F8FB52783EB248F2FBCDE7")
--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h8#j6hsf-!rble-rn#tc+k_d%u(uj&*4kmwgxl*pi5v1w1t$2u'
+# SECRET_KEY = 'django-insecure-h8#j6hsf-!rble-rn#tc+k_d%u(uj&*4kmwgxl*pi5v1w1t$2u'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
