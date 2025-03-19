176656931df9ff06e0d649b26ffc21f3e7e80104
Carrick <carrick.entwistle2001@gmail.com>
2025-03-19 10:41:41 +0000

    Enable DEBUG mode in settings and add Exercise model with migration

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description_url = models.URLField(blank=True, null=True)
+    exercise_image = models.URLField(blank=True, null=True)
+    exercise_image1 = models.URLField(blank=True, null=True)
+    muscle_group_details = models.TextField(blank=True, null=True)
+    muscle_group = models.CharField(max_length=100)
+    equipment_details = models.TextField(blank=True, null=True)
+    equipment = models.CharField(max_length=100)
+    rating = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
