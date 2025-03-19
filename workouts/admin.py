8767fc30e500cfaeccf324c9c46303eb465ecc86
Carrick <carrick.entwistle2001@gmail.com>
2025-03-19 13:30:21 +0000

    Added openpyxl dependency to requirements.txt file

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Exercise, WorkoutSession, ExerciseLog
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(SummernoteModelAdmin):
+    list_display = ('exercise_name', 'muscle_group', 'equipment', 'rating')
+    search_fields = ['exercise_name', 'muscle_group', 'equipment']
+    summernote_fields = ('description',)
+
 
 # Register your models here.
-admin.site.register(Exercise)
+# admin.site.register(Exercise)
 admin.site.register(WorkoutSession)
 admin.site.register(ExerciseLog)
